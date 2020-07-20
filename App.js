73063f841127d1c79245f8b6453bbcb1ceeaaa66
Lance <lanarmah@gmail.com>
2020-07-20 15:10:49 +0000

    updated app

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import PlayScreen from "./src/PlayScreen";
 
 export default function App() {
-  return (
+  const [play, setPlay] = useState(false);
+
+  return play ? (
+    <>
+    <PlayScreen />
+    </>
+  ) : (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.header}>Rock Paper Scissors Game</Text>
+      <Text style={styles.emoji}>🗿 📄 ✂️</Text>
+      <View style={styles.button}>
+        <Button title="PLAY" onPress={() => setPlay(true)} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +25,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#F44336",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    fontSize: 30,
+    color: '#fff'
+  },
+  emoji: {
+    fontSize: 45,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  button: {
+    marginTop: 30,
+    width: 250,
   },
 });
