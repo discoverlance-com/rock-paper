3e981dacc8f8ba66110bcdd4593f30ce8d7c2187
Lance <lanarmah@gmail.com>
2020-07-20 15:42:53 +0000

    added web build

---
+++ web-build/precache-manifest.f94bf35c2aa68d36eb7abdecbe1283c6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "15a59793bedacdd1ce50",
+    "url": "/static/js/app.00aab15d.chunk.js"
+  },
+  {
+    "revision": "66090dc4495d4ce3dfb9",
+    "url": "/static/js/runtime~app.2e9f1821.js"
+  },
+  {
+    "revision": "66902187ac2bd60c6ec5",
+    "url": "/static/js/2.5761f94f.chunk.js"
+  },
+  {
+    "revision": "44c87f6d7c36dec8b02472ef5a5737ea",
+    "url": "/static/media/rock.0c057e73.jpg"
+  },
+  {
+    "revision": "42b7acc04a8ae2f6ff38780f72a7878b",
+    "url": "/static/media/paper.c5ceb44e.jpg"
+  },
+  {
+    "revision": "8a7b7b10774f50c522b139cadf97e53c",
+    "url": "/static/media/scissors.49bc2100.jpg"
+  },
+  {
+    "revision": "f3f68bf5b7dce8acae55c25f724625dd",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "0be1637604f66ce7fb51fa7a13a80e2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e802d2644326fd9fa701f1d969a8a94a",
+    "url": "/register-service-worker.js"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "a990a8c98c379b1c14de33443329d80a",
+    "url": "/static/js/2.5761f94f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9b117048f718d88b68cd12610ad8aa3",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "f848141e2b07e3dd459ca6b19a99d1e4",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "78d31e5e7aa383eaaf2f0160bf0d5916",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "62a509c5c28f1ece377fb73f665667e7",
+    "url": "/manifest.json"
+  }
+];
